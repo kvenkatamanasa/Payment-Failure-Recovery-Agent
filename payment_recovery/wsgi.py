fc36c61a5a56a3ebdbc85e6e43c57b990c513b3b
Kammineni Venkata Manasa <kvenkatamanasa@gmail.com>
2026-09-04 21:45:09 +0530

    Update wsgi.py

--- payment_recovery/wsgi.py
+++ payment_recovery/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for payment_recovery project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payment_recovery.settings')
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "payment_recovery.settings"
+)
+
 
 application = get_wsgi_application()
